b56ad11da1e9c3249f0d2d36a068204c5d064484
Ismael Kentenich <ismaelkentenichm@gmail.com>
2021-07-12 20:28:04 -0300

    Add files via upload

---
+++ JCheckBox/src/CheckBoxTest.java
@@ -0,0 +1,15 @@
+// Testando CheckBoxFrame
+
+import javax.swing.JFrame;
+
+public class CheckBoxTest {
+	
+	public static void main( String[] args ) {
+		CheckBoxFrame checkBoxFrame = new CheckBoxFrame();
+		checkBoxFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		checkBoxFrame.setSize( 275, 100 ); // configura o tamanho do frame
+		checkBoxFrame.setVisible( true ); // exibe o frame
+		
+	}// fim de main
+	
+}// fim da classe CheckBoxTest
